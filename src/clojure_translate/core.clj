d3771c9e02d4519070729817ad910ba734bfc359
call5h5ong <Call5h5ong@gmail.com>
2022-02-24 22:50:05 +0900

    fix: handling exception to windows path separator

--- src/clojure_translate/core.clj
+++ src/clojure_translate/core.clj
@@ -4,7 +4,8 @@
   (:require [clj-http.client :as client])
   (:require [cheshire.core :as cheshire])
   (:require [clojure.walk :as walk]
-            [clojure.string :as str]))
+            [clojure.string :as str])
+  (:import (java.io File)))
 
 (defn get-file-name [file] (.getName file))
 
@@ -69,10 +70,26 @@
   [fileString]
   (str "translate-fake-" fileString))
 
+(defn exchange-windows-separator
+  "
+  윈도우와 유닉스의 path separator가 다른 것을 예외처리
+
+  윈도우의 path separator는 백슬래시를 사용하는데 이 것이 regex에서 특별하게 사용되기 때문에
+  문자로써 백슬래시를 입력하기 위해서는 연속으로 두 번 써줘야 함.
+  "
+  [separator]
+  (if (= "\\" separator)
+    "\\\\"
+    separator))
+
 (defn chop-path
   "파일 경로(/)로 문자열을 자른 lasy sequence를 리턴함"
   [filepath]
-  (str/split filepath #"/"))
+  (->>
+    (File/separator)
+    (exchange-windows-separator)
+    re-pattern
+    (str/split filepath)))
 
 (defn change-paths-filename
   [filepath toBeChanged]
@@ -93,7 +110,7 @@
 
 (defn translate-file
   "
-  들어온 파일의 이름을 번역해 새로운 io/file을 리턴함.
+  들어온 파일의 이름을 번역해 새로운 io/file을 리턴함
   "
   [translateFn file]
   (let
