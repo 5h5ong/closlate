ce92d53902e08ebb63aa6fad83e96c1fe83137ef
call5h5ong <Call5h5ong@gmail.com>
2022-03-02 18:57:02 +0900

    test: add test

--- test/closlate/core_test.clj
+++ test/closlate/core_test.clj
@@ -1,7 +1,30 @@
-(ns clojure-translate.core-test
+(ns closlate.core-test
   (:require [clojure.test :refer :all]
-            [clojure-translate.core :refer :all]))
+            [closlate.core :refer :all]))
 
 (deftest a-test
   (testing "FIXME, I fail."
-    (is (= 0 1))))
+    (is (= 1 1))))
+
+(deftest default-functon-test
+  (testing "exchange-windows-separator"
+    (is (= (exchange-windows-separator "\\") "\\\\"))
+    (is (= (exchange-windows-separator "/")  "/")))
+  (testing "translate-fake"
+    (is (= (translate-fake "test") "translate-fake-test")))
+  (testing "chop-path windows"
+    (is (= (chop-path "\\" "C:\\test\\test.txt") ["C:" "test" "test.txt"])))
+  (testing "chop-path unix linux"
+    (is (= (chop-path "/" "/Users/test/test.txt") ["" "Users" "test" "test.txt"])))
+  (testing "check-secret"
+    (is (= (check-secret ".DS_Store") false))
+    (is (= (check-secret "test.txt") true)))
+  (testing "not-exists-hashmap-keys"
+    (is (= (not-exists-hashmap-keys
+             {:test "test"  :test2 "test2"}
+             [:test :test2])
+           []))
+    (is (= (not-exists-hashmap-keys
+             {:test "test"}
+             [:test :not-exists])
+           [:not-exists]))))
